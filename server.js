68574ff84e98177808231300685241665c33d0b5
precious Nwogu <preshwhyte1@gmail.com>
2024-05-14 00:31:05 +0100

    abeg

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const http=require('node:http')
-const db=[
-
-
-]
+let db=[]
 
 
 const server=http.createServer(reqServer)
@@ -15,8 +12,61 @@ function reqServer(req,res){
     }
 
      else if(req.url==="/" && req.method==="POST"){
-        postJoke(req,res)
+        let body=[]
+
+        req.on('data',(chunk)=>{
+            body.push(chunk)
+        })
+        req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString()
+            const joke=JSON.parse(parsedBody)
+            db.push(joke)
+            res.end(JSON.stringify(db))
+            console.log(db)
+        })
      }
+     else if(req.url.split("")[0]==="/" && req.method==="PATCH"){
+        let body=[]
+
+        req.on('data',(chunk)=>{
+            body.push(chunk)
+        })
+        req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString()
+            const jokeUpdate=JSON.parse(parsedBody)
+
+            const updateID=req.url.split("")[1];
+
+            const dataIndex=db.findIndex((item)=>item.id===updateID)
+            db[dataIndex]= {...db[dataIndex], ...jokeUpdate}
+
+            res.writeHead(200)
+            res.end(JSON.stringify(jokeUpdate))
+            console.log(dataIndex)
+
+     })
+    }
+    else if(req.url.split("")[0]==="/" && req.method==="DELETE"){
+        let body=[]
+
+        req.on('data',(chunk)=>{
+            body.push(chunk)
+        })
+        req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString()
+            const jokeDelete=JSON.parse(parsedBody)
+
+            const deleteID=req.url.split("")[1];
+
+            const dataIndex=db.findIndex((item)=>item.id===deleteID)
+            db[dataIndex]= {...db[dataIndex], ...jokeUpdate}
+
+            res.writeHead(200)
+            res.end(JSON.stringify(jokeDelete))
+            console.log(dataIndex)
+
+     })
+    }
      else{
         res.writeHead(404)
         res.end(JSON.stringify({message:'not found'}))
